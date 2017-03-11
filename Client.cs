eb7dbc7697e34a8487408820393023abe2f470b0
tanya <t.evtushenko.s@yandex.by>
2017-03-11 23:41:41 +0300

    Добавьте файлы проекта.

---
+++ Client.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using ClassLibrary;
+
+namespace Crocodile
+{
+    class Client : TCPConnect
+    {
+        public string Username { get; set; }
+        private readonly MainWindow window;
+        private readonly char separator = ' ';
+
+        public Client(MainWindow wind, string username)
+        {
+            window = wind;
+            Username = username;
+        }
+
+        public void SendEnter()
+        {
+            SendStr(Commands.Enter, Username + " has joined");
+        }
+
+        public void SendLogOff()
+        {
+            SendStr(Commands.Exit, Username + " has left a game");
+        }
+
+        public void SendMessage(string message)
+        {
+            SendStr(Commands.Message, Username + ": " + message);
+        }
+
+        public void SendPoint(double x, double y)
+        {
+            SendStr(Commands.Point, x.ToString() + separator + y);
+        }
+
+        public void SendBeginPaint(double x, double y)
+        {
+            SendStr(Commands.BeginPaint, x.ToString() + separator + y);
+        }
+
+        protected override void GetReceiveData(Commands command, string str)
+        {
+            switch (command)
+            {
+                case Commands.Enter:
+                case Commands.Exit:
+                    window.PrintStr("\t" + str);
+                    break;
+                case Commands.Message:
+                    window.PrintStr(DateTime.Now.ToShortTimeString() + ", " + str);
+                    break;
+                case Commands.Point:
+                case Commands.BeginPaint:
+                    var indexSeparator = str.IndexOf(separator);
+                    var x = Convert.ToDouble(str.Substring(0, indexSeparator));
+                    var y = Convert.ToDouble(str.Substring(indexSeparator + 1, str.Length - indexSeparator - 1));
+                    if (command == Commands.Point)
+                        window.PrintPoint(x, y);
+                    else
+                    {
+                        window.LastPoint = new Point(x, y);
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using ClassLibrary;
+
+namespace Crocodile
+{
+    public partial class MainWindow : Window
+    {
+        public Point LastPoint { get; set; }
+
+        private Purpose purpose;
+        private Client client;
+        private bool isPaint;
+        private Brush color = new SolidColorBrush(Colors.Black);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GridMain.Visibility = Visibility.Hidden;
+            GridRegister.Visibility = Visibility.Visible;
+        }
+
+        private void Enter(object sender, RoutedEventArgs e)
+        {
+            client = new Client(this, TextBoxUsername.Text);
+            client.SendEnter();
+            GridMain.Visibility = Visibility.Visible;
+            GridRegister.Visibility = Visibility.Hidden;
+        }
+
+        private void ChangeParameters(object sender, TextChangedEventArgs e)
+        {
+            ButtonEnter.IsEnabled = TextBoxUsername.Text != "";
+        }
+
+        public void SetPurpose()
+        {
+            
+        }
+
+        public void StartPlay()
+        {
+            
+        }
+
+        public void PrintStr(string str)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                TextBoxMessages.Text += str + "\n";
+            });
+        }
+
+        public void PrintPoint(double x, double y)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                var line = new Line { X1 = LastPoint.X, Y1 = LastPoint.Y, X2 = x, Y2 = y, Stroke = color };
+                CanvasPaint.Children.Add(line);
+                LastPoint = new Point(x, y);
+            });
+        }
+
+        private void SendMessage(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && TextBoxEnter.Text != "")
+            {
+                client.SendMessage(TextBoxEnter.Text);
+                TextBoxEnter.Text = "";
+            }
+        }
+
+        private void SendPoint(object sender, MouseEventArgs e)
+        {
+            if (isPaint)
+            {
+                var point = e.GetPosition((Canvas)sender);
+                client.SendPoint(point.X, point.Y);
+            }
+        }
+
+        private void BeginPaint(object sender, MouseButtonEventArgs e)
+        {
+            isPaint = true;
+            var point = e.GetPosition((Canvas)sender);
+            client.SendBeginPaint(point.X, point.Y);
+        }
+
+        private void EndPaint(object sender, MouseButtonEventArgs e)
+        {
+            isPaint = false;
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            try
+            {
+                if (client != null)
+                    client.SendLogOff();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "ERROR");
+            }
+        }
+    }
+}
